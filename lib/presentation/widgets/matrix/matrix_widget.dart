de719cd1dc2de156c6735516002b5543a1d22b21
0xhacksy <hansy.schmitt.dev@gmail.com>
2020-11-12 07:28:34 -0500

    Add CenteredLoadedWidget - Refactor Matrix Widget

--- lib/presentation/widgets/matrix/matrix_widget.dart
+++ lib/presentation/widgets/matrix/matrix_widget.dart
@@ -3,8 +3,11 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test_app/domain/bloc/matrix_cubit.dart';
+
+import '../centered_loading_widget.dart';
 part 'valid_matrix_widget.dart';
 part 'invalid_matrix_widget.dart';
+part 'empty_matrix_widget.dart';
 
 class MatrixWidget extends StatelessWidget {
   @override
@@ -13,9 +16,7 @@ class MatrixWidget extends StatelessWidget {
       builder: (BuildContext context, MatrixState state) {
         switch (state.status) {
           case MatrixStatus.processing:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
+            return CenteredLoadingWidget();
             break;
           case MatrixStatus.invalid:
             return _InvalidMatrixWidget();
@@ -25,11 +26,7 @@ class MatrixWidget extends StatelessWidget {
             break;
           case MatrixStatus.empty:
           default:
-            return Container(
-              child: Center(
-                child: Text("Ingrese su matriz"),
-              ),
-            );
+            return _EmptyMatrixWidget();
             break;
         }
       },

66930eba4527996162336ef8ab0f66c9515876bd
0xhacksy <hansy.schmitt.dev@gmail.com>
2020-11-12 10:25:12 -0500

    TODO: Widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,7 @@
-
+//TODO:
+//We dont have test in this version, but we can add
+// Unit test the rotation functions
+//bloc test for valid/ invalid states
+// widget test for the textfield, floating and many grid states
+//Also , simulating adding specific values
+// integration tests simulating different states

e81caf69e00fd22cd9637a591984a201ee18f089
0xhacksy <hansy.schmitt.dev@gmail.com>
2020-11-12 09:39:59 -0500

    Add small parser for generating matrices

--- lib/domain/bloc/matrix_cubit.dart
+++ lib/domain/bloc/matrix_cubit.dart
@@ -6,9 +6,52 @@ class MatrixCubit extends Cubit<MatrixState> {
   MatrixCubit() : super(MatrixState.empty());
   List<String> matrix = [];
   void updateInput(String input) {
-    print(input);
+    if (!checkIfInputIsValid(input)) {
+      emit(MatrixState.invalid());
+      return;
+    }
+
+    var currentState = MatrixState.valid(matrix);
+    emit(currentState);
   }
 
   void rotate() {}
   void counterRotate() {}
+
+  //These functions will probably need to be on an util file
+  //Currently here b/c is not used anywhere else
+  bool checkIfInputIsValid(String input) {
+    //Reinit matrix
+    matrix = [];
+    //Remove unneded empty spaces
+    input = input.trim();
+    //Regex to match
+    RegExp matrixRegExp = RegExp(r'\[(\[((\d+)[ ]?,?)+][ ]?,?[ ]?)+\]');
+    if (!matrixRegExp.hasMatch(input)) {
+      return false;
+    }
+    //Check if it contains the number of elements needed
+    //Count how many small arrays we have
+    List<String> itemList = [];
+    for (RegExpMatch match in RegExp(r'(\[[0-9,]+\])').allMatches(input)) {
+      //Add to my tempBucket
+      itemList.add(match.group(1));
+    }
+    //Set parent size
+    int bucketSize = itemList.length;
+    for (String item in itemList) {
+      //Get all numbers
+      var matches = RegExp(r'([0-9]+)').allMatches(item);
+      //If matches length != parent Size , is invalid
+      if (matches.length != bucketSize) {
+        return false;
+      }
+      //Append all numbers to my list
+      for (RegExpMatch match in matches) {
+        matrix.add(match.group(1));
+      }
+    }
+
+    return true;
+  }
 }

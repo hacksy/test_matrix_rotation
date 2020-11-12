6bf3a75d3408dfc8fafa2d1ae0418ea7fd837b6f
0xhacksy <hansy.schmitt.dev@gmail.com>
2020-11-12 07:25:46 -0500

    Update Matrix to manage changes on bloc events

--- lib/presentation/widgets/matrix/matrix_widget.dart
+++ lib/presentation/widgets/matrix/matrix_widget.dart
@@ -1,21 +1,34 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test_app/domain/bloc/matrix_cubit.dart';
 part 'valid_matrix_widget.dart';
 part 'invalid_matrix_widget.dart';
 
 class MatrixWidget extends StatelessWidget {
-  final List<String> items;
-  MatrixWidget({this.items});
   @override
   Widget build(BuildContext context) {
-    //Lets validate that I have n x n items, otherwise , show an error message
-    if (sqrt(items.length).ceilToDouble() ==
-            sqrt(items.length).floorToDouble() &&
-        items.length > 0) {
-      return _ValidMatrixWidget(items: items);
-    } else {
-      return _InvalidMatrixWidget();
-    }
+    return BlocBuilder<MatrixCubit, MatrixState>(
+      builder: (BuildContext context, MatrixState state) {
+        switch (state.status) {
+          case MatrixStatus.processing:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+            break;
+          case MatrixStatus.invalid:
+            return _InvalidMatrixWidget();
+            break;
+          case MatrixStatus.valid:
+            return _ValidMatrixWidget(items: state.matrix);
+            break;
+          case MatrixStatus.empty:
+          default:
+            return Container();
+            break;
+        }
+      },
+    );
   }
 }

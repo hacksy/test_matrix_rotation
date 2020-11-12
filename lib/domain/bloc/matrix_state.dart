cc5489c72e716c05236d6ecb203f5f07f7c591dc
0xhacksy <hansy.schmitt.dev@gmail.com>
2020-11-12 07:22:30 -0500

    Update bloc to handle data on valid state

--- lib/domain/bloc/matrix_state.dart
+++ lib/domain/bloc/matrix_state.dart
@@ -5,14 +5,16 @@ enum MatrixStatus { empty, processing, invalid, valid }
 class MatrixState extends Equatable {
   const MatrixState._({
     this.status = MatrixStatus.empty,
+    this.matrix,
   });
   final MatrixStatus status;
-
+  final List<String> matrix;
   const MatrixState.empty() : this._(status: MatrixStatus.empty);
   const MatrixState.processing() : this._(status: MatrixStatus.processing);
   const MatrixState.invalid() : this._(status: MatrixStatus.invalid);
-  const MatrixState.valid() : this._(status: MatrixStatus.valid);
+  const MatrixState.valid(matrix)
+      : this._(status: MatrixStatus.valid, matrix: matrix);
 
   @override
-  List<Object> get props => [status];
+  List<Object> get props => [status, matrix];
 }
